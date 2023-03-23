c17da78d7ba8c93739413492753b93464848c1f9
Vee-nimation <vincentakporhuarho@gmail.com>
2023-03-23 01:13:58 -0700

    mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,20 @@
-
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""Script that lists all objects that contain the a letter from the database
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
+
+    states = session.query(State).filter(State.name.like(sys.argv[4]))
+    if states.count() != 1 or not states:
         print("Not found")
+    else:
+        print("{}".format(states.first().id))
