341002bbf6a30c1c495425bb23165e1e977ec2dd
“Vee-nimation” <vincentakporhuarho@gmail.com>
2023-03-31 18:00:34 -0700

    network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
-
-
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 import urllib.request
 
 
+def main():
+    """
+    Funtion to print a response of a specific url
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        content = the_page.decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(content))
+    main()
